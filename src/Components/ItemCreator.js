45d9acdbe70f353e5efdc55dc65b9beb1d9f96cd
justinmrks <justinmarks19@gmail.com>
2022-04-20 19:53:30 -0500

    added funttionality to creating and displaying list items

--- src/Components/ItemCreator.js
+++ src/Components/ItemCreator.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import listState from './Atoms';
 
 const ItemCreator = () => {
-  return <div>ItemCreator</div>;
+  const [todoList, setTodoList] = useRecoilState(listState);
+  const [input, setInput] = useState('');
+
+  const change = (event) => {
+    setInput(event.target.value);
+  };
+
+  const submit = () => {
+    setTodoList([
+      ...todoList,
+      {
+        id: getId(),
+        item: input,
+        completed: false,
+      },
+    ]);
+    setInput('');
+    console.log('ping', todoList);
+  };
+
+  return (
+    <div>
+      <input type="text" value={input} onChange={change} />
+      <input type="submit" value="submit" onClick={submit} />
+    </div>
+  );
+};
+
+let id = 0;
+
+const getId = () => {
+  id += 1;
+  return id;
 };
 
 export default ItemCreator;
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import listState from './Atoms';
 
 const List = () => {
-  return <div>List</div>;
+  const [todoList, setTodoList] = useRecoilState(listState);
+
+  return (
+    <>
+      {todoList.length < 1 ? (
+        <p>Please Add an Item to Your Todo List</p>
+      ) : (
+        todoList.map((item, key) => {
+          return (
+            <div>
+              {item.item}
+              <input
+                type="checkbox"
+                name="completion"
+                checked={item.completed}
+              />
+            </div>
+          );
+        })
+      )}
+    </>
+  );
 };
 
 export default List;

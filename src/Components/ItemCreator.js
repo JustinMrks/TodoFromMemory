3d46de239c794a4f5bdbbe29fd32d2de1067ba4b
justinmrks <justinmarks19@gmail.com>
2022-05-07 00:33:00 -0500

    edits to the todo item creation form

--- src/Components/ItemCreator.js
+++ src/Components/ItemCreator.js
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import listState from './Atoms';
+import axios from 'axios';
 
 const ItemCreator = () => {
+  const defaultForm = { name: '', description: '' };
   const [todoList, setTodoList] = useRecoilState(listState);
-  const [input, setInput] = useState('');
+  const [form, setForm] = useState(defaultForm);
 
-  const change = (event) => {
-    setInput(event.target.value);
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
   };
 
-  // const submit = () => {
-  //   setTodoList([
-  //     ...todoList,
-  //     {
-  //       id: getId(),
-  //       item: input,
-  //       completed: false,
-  //     },
-  //   ]);
-  //   setInput('');
-  //   console.log('ping', todoList);
-  // };
+  const submit = () => {
+    axios.post('', form).then().catch();
+    setForm(defaultForm);
+    console.log('ping', todoList);
+  };
+  // ^^^^^^Make this work^^^^^^
 
   return (
     <div>
-      <input type="text" value={input} onChange={change} />
+      <input
+        type="text"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="description"
+        value={form.description}
+        onChange={handleChange}
+      />
       <input type="submit" value="submit" onClick={submit} />
     </div>
   );
-};
-
-let id = 0;
-
-const getId = () => {
-  id += 1;
-  return id;
 };
 
 export default ItemCreator;
